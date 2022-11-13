5e1f832d6803bdc990da85f77665f8039f24183d
sahil <79046384+SahilLohan@users.noreply.github.com>
2022-11-13 18:16:51 +0530

    Create Solution.cpp

---
+++ 844. Backspace String Compare/Solution.cpp
@@ -0,0 +1,52 @@
+//link:-https://leetcode.com/problems/implement-queue-using-stacks/description/
+
+//SOLUTION:-class MyQueue {
+public:
+    stack<int> real;
+    stack<int> temp; 
+    
+    void push(int x) {
+        if(real.empty())
+        {
+            real.push(x);
+        }
+        else
+        {
+            while(!real.empty())
+            {
+                temp.push(real.top());
+                real.pop();
+            }
+            real.push(x);
+            while(!temp.empty())
+            {
+                real.push(temp.top());
+                temp.pop();
+            }
+        }
+    }
+    
+    int pop() {
+
+        int ans = real.top();
+        real.pop();
+        return ans;
+    }
+    
+    int peek() {
+        return real.top();
+    }
+    
+    bool empty() {
+        return real.empty();
+    }
+};
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue* obj = new MyQueue();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->peek();
+ * bool param_4 = obj->empty();
+ */
