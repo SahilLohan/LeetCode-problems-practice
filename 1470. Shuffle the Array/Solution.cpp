a760f76fe24515d840bfae8af72a9c7595770a89
sahil <79046384+SahilLohan@users.noreply.github.com>
2023-02-10 07:19:38 +0530

    Create Solution.cpp

---
+++ 1470. Shuffle the Array/Solution.cpp
@@ -0,0 +1,15 @@
+// Link :- https://leetcode.com/problems/as-far-from-land-as-possible/
+
+class Solution {
+public:
+    vector<int> shuffle(vector<int>& nums, int n) {
+        vector<int> ret;
+
+        for(int i=0;i<n;i++)
+        {
+            ret.push_back(nums[i]);
+            ret.push_back(nums[n+i]);
+        }
+        return ret;
+    }
+};
