6e0667403a83ab272259c9090f509c3d8726b318
Sahil Lohan <79046384+SahilLohan@users.noreply.github.com>
2023-07-14 21:09:51 +0530

    Create Solution.cpp

---
+++ 141. Linked List Cycle/Solution.cpp
@@ -0,0 +1,35 @@
+// Link : https://leetcode.com/problems/linked-list-cycle/
+
+
+// Code :
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        ListNode *fast=head;
+        ListNode *slow=head;
+
+        while(fast!= nullptr && slow!=nullptr)
+        {
+            slow = slow->next;
+            fast = fast->next;
+            if(fast == nullptr)
+            {
+                return false;
+            }
+            else
+            fast = fast->next;
+
+            if(slow == fast) return true;
+        }
+
+        return false;
+    }
+};
