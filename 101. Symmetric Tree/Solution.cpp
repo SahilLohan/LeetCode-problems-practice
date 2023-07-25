f1780b67308e853cec080a40cc6e00392d43fad4
Sahil Lohan <79046384+SahilLohan@users.noreply.github.com>
2023-07-25 19:29:57 +0530

    Create Solution.cpp

---
+++ 101. Symmetric Tree/Solution.cpp
@@ -0,0 +1,33 @@
+// Link : https://leetcode.com/problems/symmetric-tree/
+
+
+// Code :
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool solve(TreeNode* a,TreeNode* b)
+    {
+        if(!a && !b) return true;
+        if(!a || !b) return false;
+
+        return (a->val == b->val && solve(a->left,b->right) && solve(a->right,b->left));
+    }
+
+    bool isSymmetric(TreeNode* root) {
+        if(!root) return true;
+        if(!root->left && !root->right) return true;
+        if(!root->left || !root->right) return false;
+
+        return solve(root->left , root->right);
+    }
+};
